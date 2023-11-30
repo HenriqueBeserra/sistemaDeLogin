56613f9741fc0365751895f902673839a0c8c71a
Henrique Beserra <millerhiquinho@gmail.com>
2023-11-30 08:34:07 -0300

    Arquivo de rotas desenvolvido - aguardando mais opções de funcionalidades

--- back-end/src/routes/router.js
+++ back-end/src/routes/router.js
@@ -5,9 +5,10 @@ const models = require('../models/tasksModels')
 const controlers = require('../controller/tasksControler')
 
 router.get('/config', (req, res) => {res.send("API RODANDO NORMAL")});
-router.get('/g', controlers.verificaUsuario)
+router.get('/g', controlers.verificaUsuario);
 router.post('/us', controlers.adicionaUsuario);
-router.put('/p/:id', controlers.alteraUsuario)
+router.put('/p/:id', controlers.alteraUsuario);
+router.delete('/d/:id', controlers.apagaUsuario)
 
 
 
